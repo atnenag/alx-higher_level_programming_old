bf01ba5aea3cc37ca4535ad6d406a96bc2a11df4
Atnafu <atnenag@gmail.com>
2023-03-20 10:30:43 -0700

    loop again correcion

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number%3 == 0) & (number%5==0):
+            print("fizzbuzz", end = " ")
+        elif number%3 == 0:
+            print("Fizz", end = " ")
+        elif number%5 == 0:
+            print("buzz", end = " ")
+        else:
+            print("{}".format(number), end = " ")
