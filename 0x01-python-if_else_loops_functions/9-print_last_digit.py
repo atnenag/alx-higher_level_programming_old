d4ecdd165e215ba5b04503820a0461eb6150fe43
Atnafu <atnenag@gmail.com>
2023-03-24 06:15:01 -0700

    101-remove.py & 102-magic.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number >=0:
-        last_digit = number % 10
+    if number <0:
+        return (number % 10) * -1
     else:
-        last_digit = ((number * -1) % 10) *-1
-    return last_digit
+        return number % 10
