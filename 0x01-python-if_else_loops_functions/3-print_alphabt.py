08daf29d7a2814c79dd9eab82897046e92592a13
Atnafu <atnenag@gmail.com>
2023-03-24 06:44:21 -0700

    lower_letter2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for low_letter2 in range(97, 12):
+for low_letter2 in range(97, 123):
     if (low_letter2 != 101 and low_letter2 != 113):
         print("{}".format(chr(low_letter2)), end='')
