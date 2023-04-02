7a6a22f8aa663cf47354b5cbbcc88eca1713457b
Atnafu <atnenag@gmail.com>
2023-04-02 14:42:01 -0700

    0-square_matrix_simple.py 2nd chance

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    return dict(sorted(a_dictionary.items()))
+def print_sorted_dictionary(my_dict):
+    for keys in sorted(my_dict.keys()):
+        print('{}: {}'.format(keys, my_dict[keys]))
