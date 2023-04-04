887d22571d7af09c0832f1f3ddc727c00d0de701
Atnafu <atnenag@gmail.com>
2023-04-04 07:16:17 -0700

    0x04-python-more_data_structures final phase 2nd

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is not:
-        return list(map(lambda (x, y): (float(9)/5)*x + 32, C))
+    if not my_list:
+        return 0
+    weight_total = 0
+    weight_score_sum = 0
+    for score, weight in my_list:
+        weight_total += weight
+        weight_score_sum += score * weight
+    return weight_score_sum / weight_total
