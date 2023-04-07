15e98092def7bfec2b615141b4413900e9eb8544
Atnafu <atnenag@gmail.com>
2023-04-07 01:07:57 -0700

    0x05-python-exceptions too late assignment (1-safe_print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if type(value) is int:
-            print("{:d}".format(value))
-            return True
-        else:
-            return value is int
-    except SyntaxError:
-        pass
-    print()
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
